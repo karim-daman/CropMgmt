37d958074caa73cb60ebcdcc2f417ed78b9a39b7
karim-daman <38837234+karim-daman@users.noreply.github.com>
2025-02-22 23:18:14 +0100

    update

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { checkForAppUpdates } from '../../lib/updater';
+
+	async function checkForUpdates() {
+		await checkForAppUpdates(false);
+	}
+</script>
+
+<button onclick={checkForUpdates}>check for updates</button>
